18c2442cf6e636fe6f60d8a08ededb755dff12af
ziv <ziv@larvata.tw>
2020-12-02 16:01:37 +0800

    feat: 安裝 vue 和 vue-ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,12 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
+import Vue from 'vue'
+import App from '../app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    render: h => h(App)
+  }).$mount('#vue-app')
+})
