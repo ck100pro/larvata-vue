25ca29407fe7cb05c031c6e057adbd596e0ed5df
ziv <ziv@larvata.tw>
2020-12-03 15:56:17 +0800

    feat: 新增 org 表單和建立假資料

---
+++ db/migrate/20201203060550_add_org_table.rb
@@ -0,0 +1,10 @@
+class AddOrgTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orgs do |t|
+      t.string :name, default: ''
+    
+      t.timestamps
+    end
+    
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+orgs = [
+  { name: '大聯盟' },
+  { name: '小聯盟' },
+  { name: '種花電信' },
+  { name: 'A錢中油' },
+  { name: '台Ｇ電' }
+]
+
+orgs.each do |org|
+  Org.find_or_create_by(name: org[:name])
+end
