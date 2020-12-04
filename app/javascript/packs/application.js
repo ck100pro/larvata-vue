77bc4680c3d7e6cdf067095f4f290c3db8651f25
ziv <ziv@larvata.tw>
2020-12-04 11:17:18 +0800

    feat: 安裝 axios 和預設字體

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,18 @@ require("channels")
 import Vue from 'vue'
 import App from '../app.vue'
 import Vuetify from 'vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
+Vue.use(VueAxios, axios)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    vuetify: new Vuetify(),
+    vuetify: new Vuetify({ icons: { iconfont: 'mdi' }}),
     render: h => h(App)
   }).$mount('#vue-app')
 })
