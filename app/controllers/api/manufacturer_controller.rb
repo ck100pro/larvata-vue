e5aa5a45757bb497f23b6cd4eb43679c3be93e2f
ziv <ziv@larvata.tw>
2021-01-13 09:55:11 +0800

    feat: 修正 controller

--- app/controllers/api/manufacturer_controller.rb
+++ app/controllers/api/manufacturer_controller.rb
@@ -4,4 +4,13 @@ class Api::ManufacturerController < ApplicationController
     render json: Manufacturer.select(:id, :name).map(&:to_select)
   end
 
+  def datatables
+    length = params[:length]
+    start = params[:start]
+    manufacturers_count = Manufacturer.count
+    manufacturers = Manufacturer.offset(start).limit(length).map(&:to_datatable)
+
+    render json: { count: manufacturers_count, data: manufacturers }
+  end
+
 end
--- app/controllers/manufacturer_controller.rb
+++ app/controllers/manufacturer_controller.rb
@@ -1,6 +1,4 @@
 class ManufacturerController < ApplicationController
   def index
-    # render json: Manufacturer.select(:id, :name).map(&:to_select)
-    render json: Manufacturer.all.map(&:to_datatable)
   end
 end
