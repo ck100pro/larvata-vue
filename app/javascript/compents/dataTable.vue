48537fc8cd19f1312d09c784468d0dd2261b4922
Ck100pro <ck100protf10@gmail.com>
2021-01-13 18:57:45 +0800

    feat: 新增 data_table 的操作區域
    1. 增加 options 的 templete
    2. 增加 options 的邏輯操作

--- app/javascript/compents/dataTable.vue
+++ app/javascript/compents/dataTable.vue
@@ -5,97 +5,140 @@
             :items="items"
             :items-per-page="itemsPerPage"
             :page.sync="page"
+            :loading='loading'
             hide-default-footer
             loading-text="資料讀取中請稍候"
             item-key="name"
             class="elevation-1"
-        ></v-data-table>
+        >
+          <template v-slot:[`item.options`]="{ item }">
+            <larvata-data-table-options
+            :item="item"
+            ></larvata-data-table-options>
+          </template>
+        </v-data-table>
 
-        <v-pagination
-          v-model="page"
-          :length="pageCount"
-          :total-visible="10"
-          @input="pagination"
-        ></v-pagination>
+        <div class="text-center pt-2">
+          <v-pagination
+            v-model="page"
+            :length="pageCount"
+            :total-visible="totalVisible"
+            @input="inputPagination"
+          ></v-pagination>
+
+          <v-text-field
+            :value="itemsPerPage"
+            label="Items per page"
+            type="number"
+            :min="perPageMin"
+            :max="perPageMax"
+            @input="inputPerPage"
+          ></v-text-field>
+      </div>
     </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      items: [],
-      itemsPerPage: 10,
-      page: 1,
-      pageCount: 0,
-      start: 0
-    }
-  },
-  computed: {
-    headers () {
-      return [
-        {
-          text: '建立日期',
-          value: 'created_at',
-        },
-        {
-          text: '組織名稱',
-          value: 'name',
-          sortable: false
-        },
-        {
-          text: '負責人',
-          value: 'principal',
-          sortable: false
-        },
-        { 
-          text: '編碼',
-          value: 'code',
-          sortable: false
-        },
-        { 
-          text: '電話',
-          value: 'tel',
-          sortable: false
-        },
-        { 
-          text: '信箱',
-          value: 'email',
-          sortable: false
-        },
-        { 
-          text: '結帳日',
-          value: 'account_day',
-          sortable: false
-        },
-      ]
+  import larvataDataTableOptions from './options'
+
+  export default {
+    data () {
+      return {
+        items: [],
+        itemsPerPage: 10,
+        page: 1,
+        pageCount: 0,
+        start: 0,
+        totalVisible: 10,
+        perPageMax: 20,
+        perPageMin: 5,
+        loading: false
+      }
     },
-  },
-  methods: {
-    call_api() {
-      this.axios.get('api/manufacturer/datatables',{
-        params: {
-          start: this.start,
-          length: this.itemsPerPage
+    computed: {
+      headers () {
+        return [
+          {
+            text: '操作',
+            value: 'options',
+            sortable: false
+          },
+          {
+            text: '建立日期',
+            value: 'created_at',
+            sortable: false
+          },
+          {
+            text: '組織名稱',
+            value: 'name',
+            sortable: false
+          },
+          {
+            text: '負責人',
+            value: 'principal',
+            sortable: false
+          },
+          { 
+            text: '編碼',
+            value: 'code',
+            sortable: false
+          },
+          { 
+            text: '電話',
+            value: 'tel',
+            sortable: false
+          },
+          { 
+            text: '信箱',
+            value: 'email',
+            sortable: false
+          },
+          { 
+            text: '結帳日',
+            value: 'account_day',
+            sortable: false
+          },
+        ]
+      },
+    },
+    methods: {
+      datatableAttr() {
+        this.loading = true
+        this.axios.get('api/manufacturer/datatables',{
+          params: {
+            start: this.start,
+            length: this.itemsPerPage
+          }
+        }).then((response) => {
+          console.log(response.data)
+          this.items = response.data.data
+          this.pageCount = Math.ceil(response.data.count / this.itemsPerPage)
+        }).catch((error) => {
+
+        }).then(() => {
+          this.loading = false
+        })
+      },
+      inputPagination(e) {
+        this.page = parseInt(e, 10)
+        this.start = (this.page - 1) * this.itemsPerPage
+        this.datatableAttr()
+      },
+      inputPerPage(e) {
+        let inputNum = parseInt(e, 10)
+        if (inputNum > this.perPageMax) {
+          this.itemsPerPage = this.perPageMax
+        } else if (inputNum < this.perPageMin) {
+          this.itemsPerPage = this.perPageMin
+        } else {
+          this.itemsPerPage = inputNum
         }
-      }).then((response) => {
-        console.log(response.data)
-        this.items = response.data.data
-        this.pageCount = response.data.count / this.itemsPerPage
-      }).catch((error) => {
-
-      }).then(() => {
-
-      })
+        this.datatableAttr()
+      }
     },
-    pagination(page) {
-      this.page = page
-      this.start = this.start
-      console.log(page)
-    }
-  },
-  created: function() {
-    this.call_api()
+    created: function() {
+      this.datatableAttr()
+    },
+    components: { larvataDataTableOptions }
   }
-}
 </script>
---
+++ app/javascript/compents/options.vue
@@ -0,0 +1,47 @@
+<template >
+    <div>
+        <v-icon
+            small
+            class="mr-2"
+            @click="showItem()"
+        >
+        mdi-eye
+        </v-icon>
+        <v-icon
+            small
+            class="mr-2"
+            @click="editItem()"
+        >
+        mdi-pencil
+        </v-icon>
+        <v-icon
+            small
+            @click="deleteItem()"
+        >
+        mdi-delete
+        </v-icon>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            item: Object
+        },
+        methods: {
+            showItem() {
+                console.log(this.item)
+            },
+            editItem() {
+                console.log(this.item)
+            },
+            deleteItem () {
+                console.log(this.item)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
